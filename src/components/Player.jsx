6893ed0dba14e1e653476544fb8a739da7472802
cravsky <cravsky@gmail.com>
2024-04-03 12:21:49 +0200

    Implemented 2 way binding

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,19 +1,27 @@
 import { useState } from 'react';
 
-export default function Player({ name, symbol }) {
+export default function Player({ initialName, symbol }) {
 
     const [isEditing, setIsEditing] = useState(false);
+    const [playerName, setPlayerName] = useState(initialName);
 
     function handleEditClick() {
         setIsEditing(editing => !editing); // schedules a state update to the opposite of the current value of isEditing
+    }
+
+    function handleChange(event) { // event object is passed to the event handler function automatically
+        setPlayerName(event.target.value);
     }
 
     return (
         <li>
             <span className="player">
                 {isEditing
-                    ? <input type='text' required value={name}></input>
-                    : <span className="player-name">{name}</span>}
+                    ? <input type='text'
+                        required
+                        value={playerName}
+                        onChange={handleChange}></input>
+                    : <span className="player-name">{playerName}</span>}
                 <span className="player-symbol">{symbol}</span>
             </span>
             <button onClick={handleEditClick}>{
