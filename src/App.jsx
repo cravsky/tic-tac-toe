c33aea787f68481a19a9077e342c4a80cab3f78f
cravsky <cravsky@gmail.com>
2024-04-08 17:31:49 +0200

    Implemented rematch button

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,6 @@ const initialGameBoard = [
 ]
 
 function deriveActivePlayer(gameTurns) {
-  // KK simplification
   return gameTurns.length % 2 === 0 ? 'X' : 'O';
 }
 
@@ -24,7 +23,7 @@ function App() {
 
   const activePlayer = deriveActivePlayer(gameTurns);
 
-  let gameBoard = initialGameBoard;
+  let gameBoard = [...initialGameBoard.map(array => [...array])];
 
   for (const turn of gameTurns) {
     const { square, player } = turn;
@@ -66,6 +65,10 @@ function App() {
     })
   }
 
+  function handleRestart() {
+    setGameTurns([]);
+  }
+
   return (
     <main>
       <div id="game-container">
@@ -73,11 +76,14 @@ function App() {
           <Player initialName="Player 1" symbol="X" isActive={activePlayer === 'X'} />
           <Player initialName="Player 2" symbol="O" isActive={activePlayer === 'O'} />
         </ol>
-        {(winner || hasDraw) && <GameOver winner={winner}/>}
+        {(winner || hasDraw) &&
+          <GameOver winner={winner} onRestart={handleRestart} />}
         <GameBoard
           onSelectSquare={handleSelectSquare}
           board={gameBoard}
-          turns={gameTurns} />
+          turns={gameTurns}
+
+        />
       </div>
       <Log turns={gameTurns} />
     </main>
