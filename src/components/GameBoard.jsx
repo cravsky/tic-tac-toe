7ef4037a32025160a04c762a1e8b747821c5bcc0
cravsky <cravsky@gmail.com>
2024-04-08 09:54:18 +0200

    Disabled clicked buttons

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -22,7 +22,9 @@ export default function GameBoard({ onSelectSquare, turns }) {
             {gameBoard.map((row, rowIndex) => <li key={rowIndex}>
                 <ol>
                     {row.map((playerSymbol, colIndex) => <li key={colIndex}>
-                        <button onClick={() => onSelectSquare(rowIndex, colIndex)}>{playerSymbol}</button>
+                        <button
+                            disabled={playerSymbol !== null}
+                            onClick={() => onSelectSquare(rowIndex, colIndex)}>{playerSymbol}</button>
                     </li>)}
                 </ol>
             </li>)}
